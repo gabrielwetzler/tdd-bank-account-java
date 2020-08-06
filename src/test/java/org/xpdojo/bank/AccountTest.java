2a25ea80e8f7b657594bf99de80608110ffb56a8
gabrielwetzler <gabriel.wetzler@gmail.com>
2020-08-06 11:55:47 +0200

    first test

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,6 +1,5 @@
 package org.xpdojo.bank;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -9,8 +8,13 @@ import static org.junit.Assert.assertThat;
 public class AccountTest {
 
     @Test
-	@Ignore
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test isn't implemented", true, is(false));
+    }
+
+    @Test
+    public void newAccountShouldHaveZeroBalance(){
+        Account account = new Account();
+        assertThat("Balance not 0", account.balance() == 0, is(true));
     }
 }
